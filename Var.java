3d6a7db3c2adddc6f084b5dae589a0f2da1db6f4
Travis Hahn <tjxhahn@gmail.com>
2019-05-28 22:26:04 -0400

    Update Var.java

--- Var.java
+++ Var.java
@@ -1,1 +1,14 @@
+public class Var {
+  public String name;
+  public String type;
+  public String value;
 
+  public Var(String name, String type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
