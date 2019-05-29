605379bccd99cd0922ea7c57d26d31a896eeb413
Travis Hahn <tjxhahn@gmail.com>
2019-05-28 22:17:07 -0400

    Create Line.java

---
+++ Line.java
@@ -0,0 +1,11 @@
+public class Line {
+  public String command;
+  public String type;
+  public String additionalData;
+
+  public Line(String command, String type, String additionalData) {
+    this.command = command;
+    this.type = type;
+    this.additionalData = additionalData;
+  }
+}
