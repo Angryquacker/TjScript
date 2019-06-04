fd1ebba7d236398f0db8a9e64ef6bd62176ca99c
Travis Hahn <tjxhahn@gmail.com>
2019-06-04 10:33:07 -0400

    Update Line.java

--- Line.java
+++ Line.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+
 public class Line {
   public String command;
   public String type;
   public String additionalData;
+  public int loopStart;
+  public int loopEnd;
 
-  public Line(String command, String type, String additionalData) {
+
+  public Line(String command, String type, String additionalData, int loopStart, int loopEnd) {
     this.command = command;
     this.type = type;
     this.additionalData = additionalData;
+    this.loopStart = loopStart;
+    this.loopEnd = loopEnd;
   }
 }
